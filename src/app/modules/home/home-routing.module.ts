66f1e69a83bbb17e61c204d010f8ef2870c2505d
AchinoamVizo <88616249+AchinoamVizo@users.noreply.github.com>
2021-10-13 16:13:47 +0300

    create login

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -8,4 +8,5 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
 export class HomeRoutingModule { }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ForgotComponent } from './pages/forgot/forgot.component';
+import { IndexComponent } from './pages/index/index.component';
+import { Routes,RouterModule } from '@angular/router';
 
-
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    children: [
+      { path: 'register', component: SignupComponent },
+      { path: 'login', component: SigninComponent },
+      { path: 'forgot', component: ForgotComponent },
+      { path: 'signin', component: IndexComponent },
+    ],
+  },
+];
 @NgModule({
   declarations: [
     LoginComponent,
+    SigninComponent,
+    SignupComponent,
+    ForgotComponent,
+    IndexComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule,RouterModule],
 })
-export class LoginModule { }
+export class LoginModule {}
